e66309bf123f544f4471871570f7c25b85bf8a09
IshmaelBelghazi <ishar667@gmail.com>
2015-03-17 22:04:14 -0400

    added gnutls dependencies for devtools

--- scripts/travis/travis_install.sh
+++ scripts/travis/travis_install.sh
@@ -12,3 +12,7 @@ rm -f $CUDA_FILE
 apt-get -y update
 apt-get -y install cuda-core-6-5 cuda-cublas-6-5 cuda-cublas-dev-6-5 cuda-cudart-6-5 cuda-cudart-dev-6-5 cuda-curand-6-5 cuda-curand-dev-6-5
 ln -s /usr/local/cuda-6.5 /usr/local/cuda
+
+## Curl dependencies
+apt-get -y build-dep libcurl4-gnutls-dev
+apt-get -y install libcurl4-gnutls-dev
